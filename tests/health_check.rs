6a6b2ba23b69580db53b73733c723592f8d37494
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-09 14:50:02 +0800

    done - Implementing Our First Integration Test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,7 +6,7 @@
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app().await.expect("Failed to spawn our app.");
+    spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
@@ -23,7 +23,14 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-// Launch our application in the background ~somehow~
-async fn spawn_app() -> std::io::Result<()> {
-    zero2prod::run().await
+// No .await call, therefore no need for `spawn_app` to be async now.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
 }
